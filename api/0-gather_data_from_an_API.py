3b0bb0ff4f9fd955078972cd46814e4d12083579
root <carlosfrancoart@gmail.com>
2023-12-20 22:01:55 +0000

    api v1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def get_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
