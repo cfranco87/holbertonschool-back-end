5da360601b79cf8c4e33db6fc638bf5603a89f6e
root <carlosfrancoart@gmail.com>
2023-12-21 03:01:02 +0000

    api v10

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module that gets information of an employee's TODO list progress
+based on the ID given and converts it to CSV format"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    uid = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    task_url = user_url + "/todos"
+
+    """Get the JSON dictionaries for requested user and all TODOs"""
+    user = requests.get(user_url).json()
+    todos = requests.get(task_url).json()
+
+    username = user.get("username")
+
+    """Create a list to later convert to JSON format"""
+    dict_list = []
+    for task in todos:
+        row = {}
+        row["task"] = task.get("title")
+        row["completed"] = task.get("completed")
+        row["username"] = username
+        dict_list.append(row)
+    json_dict = {}
+    json_dict[uid] = dict_list
+
+    """Convert list to JSON file"""
+    with open("{}.json".format(uid), "w") as json_file:
+        json_file.write(json.dumps(json_dict))
---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module that gets information of an employee's TODO list progress
+based on the ID given and converts it to CSV format"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users"
+
+    """Get the JSON dictionaries for users"""
+    users = requests.get(user_url).json()
+    json_dict = {}
+
+    """Create a list to later add it to a dict to convert to JSON format"""
+    for user in users:
+        uid = user.get('id')
+        task_url = user_url + "/{}/todos".format(uid)
+        todos = requests.get(task_url).json()
+        username = user.get("username")
+        dict_list = []
+        for task in todos:
+            task_dict = {}
+            task_dict["username"] = username
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            dict_list.append(task_dict)
+        json_dict[uid] = dict_list
+
+    """Convert list to JSON file"""
+    with open("todo_all_employees.json".format(uid), "w") as json_file:
+        json_file.write(json.dumps(json_dict))
