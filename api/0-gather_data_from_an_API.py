c2d88ed88763079b1f58747dbb5749f72883deaa
Carlos Franco <6752@hbtn.uy>
2024-02-25 19:05:47 -0300

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,21 @@
 #!/usr/bin/python3
+"""function to gather the data from API"""
+
 import requests
 from sys import argv
 
-"""Module using api to get to do list from employee"""
-"""module that to access command line args"""
-"""check its the main file and not an import"""
-if __name__ == "__main__":
-    """retrieve command line arg when running script"""
-    uid = argv[1]
 
-    """counter variables to 0 and starts counting from there"""
-    TOTAL_NUMBER_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = 0
+if __name__ == '__main__':
 
-    """Get JSON doct for requested user and all TODOs"""
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(uid)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    api_url = f'https://jsonplaceholder.typicode.com/'
 
-    """retrieves the value associated with the key "name" from dict 'user'
-    using the .get() method"""
-    EMPLOYEE_NAME = user.get("name")
+    user_id = (argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    task_todo = requests.get(api_url + f'users/{user_id}/todos').json()
+    completed_task = [task for task in task_todo if task['completed']]
 
-    """Get all tasks and completed tasks numbers, and the user completed task.
-    We start with empty list [], if loop then once user id is verified we then
-    add +1 to counter variables until completed task"""
-    completed_tasks = []
-    for task in todos:
-        if int(uid) == task.get('userId'):
-            TOTAL_NUMBER_OF_TASKS += 1
-            if task.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
-                completed_tasks.append(task)
+    print(f'Employee {user_data["name"]} is done with', end='')
+    print(f' task({len(completed_task)}/{len(task_todo)}):')
 
-    print('Employee {} is done with tasks({}/{}):'.format(
-        EMPLOYEE_NAME,
-        NUMBER_OF_DONE_TASKS,
-        TOTAL_NUMBER_OF_TASKS
-    ))
-
-    """Print all completed tasks titles"""
-    for task in completed_tasks:
-        print('\t {}'.format(task.get('title')))
+    for tasks in completed_task:
+        print("\t" + tasks["title"])
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,43 +1,31 @@
 #!/usr/bin/python3
-import csv
+"""
+   export the data to Json.
+"""
+
+import json
 import requests
 from sys import argv
 
-"""-CSV format for storing tubalar data(number and text)"""
-"""check its the main file and not an import"""
-if __name__ == "__main__":
-    """retrieve command line arg when running script"""
-    uid = argv[1]
-    """fetches the user data in JSON format from the specified URL"""
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    task_url = user_url + "/todos"
+if __name__ == '__main__':
+    """
+        api function
+    """
 
-    """Get the JSON dict for requested user and todos"""
-    user = requests.get(user_url).json()
-    todos = requests.get(task_url).json()
+    api_url = f'https://jsonplaceholder.typicode.com/'
 
-    """variables for each string represents a column header in the CSV file"""
-    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    username = user.get("username")
+    user_id = (argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    task_todo = requests.get(api_url + f'users/{user_id}/todos').json()
 
-    """Create a list to later convert to csv format"""
-    csv_list = []
-    for task in todos:
-        """empty dict for row on table"""
-        row = {}
-        """ organizes the data retrieved from the user's TODO list
-        into a structure suitable for CSV export"""
-        row["USER_ID"] = task.get("userId")
-        row["USERNAME"] = username
-        row["TASK_COMPLETED_STATUS"] = task.get("completed")
-        row["TASK_TITLE"] = task.get("title")
-        """adds dict to list"""
-        csv_list.append(row)
+    task_dict = []
+    for task in task_todo:
+        completed_tasks = {
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": user_data["username"]
+            }
+        task_dict.append(completed_tasks)
 
-    """codeblock responsible with opening CSV file"""
-    with open("{}.csv".format(uid), "w") as csv_file:
-        """Its for designed for writing dictionaries to CSV files"""
-        csv_ins = csv.DictWriter(csv_file, fieldnames=fieldnames,
-                                 quoting=csv.QUOTE_ALL)
-        """line writes multiple rows to the CSV file"""
-        csv_ins.writerows(csv_list)
+        with open(f'{user_id}.json', 'w') as file:
+            json.dump({user_id: task_dict}, file)
