6b42eddde18a3dfcfbcda20f3b85f02acef46460
Carlos Franco <carlosfrancoart@gmail.com>
2024-02-25 19:17:34 -0300

    two

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-""" USING A REST API THAT RETURN INFORMATION """
-import csv
+"""
+   export the data to Json.
+"""
+
+import json
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    """ prints info about employee """
+if __name__ == '__main__':
+    """
+        api function
+    """
 
-    employee = requests.get("http://jsonplaceholder.typicode.com/users?id={}"
-                            .format(argv[1]))
-    all_tks_user = requests.get(
-        "http://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    try:
-        name = employee.json()
-        all_t = all_tks_user.json()
+    api_url = f'https://jsonplaceholder.typicode.com/'
 
-    except ValueError:
-        print("Not Valid JSON")
+    user_id = (argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    task_todo = requests.get(api_url + f'users/{user_id}/todos').json()
 
-    user = name[0].get("username")
+    task_dict = []
+    for task in task_todo:
+        completed_tasks = {
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": user_data["username"]
+            }
+        task_dict.append(completed_tasks)
 
-    _file = argv[1] + ".csv"
-
-    with open(_file, "w") as f:
-        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-
-        for task in all_t:
-            title = task.get("title")
-            complete = task.get("completed")
-            id_user = task.get("userId")
-            csv_writer.writerow([id_user, user, complete, title])
+        with open(f'{user_id}.json', 'w') as file:
+            json.dump({user_id: task_dict}, file)
